f664a7450797bc8653ef2f1788cf03dad6c68694
Hasan AYDOĞDU <haydogdu1990@gmail.com>
2023-05-31 22:04:21 +0300

    Add files via upload

---
+++ pages/FoodList/FoodList.style.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: "red",
+  },
+
+  text: {
+    color: "red",
+  },
+
+  image: {
+    width: 100,
+    height: 100,
+  },
+});
