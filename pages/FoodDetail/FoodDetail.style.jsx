c3f0156b844e078080433cd0e85c17aaa4afa339
Hasan AYDOĞDU <haydogdu1990@gmail.com>
2023-05-31 22:05:22 +0300

    Delete pages directory

--- pages/FoodDetail/FoodDetail.style.jsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Dimensions } from "react-native";
-
-export default StyleSheet.create({
-  container: {},
-  image: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height / 3,
-  },
-});
